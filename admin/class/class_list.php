9dd0dce0e1f8179c96e013c472cc8df7cf7beb05
jijihui22 <96766525+jijihui22@users.noreply.github.com>
2023-08-28 17:01:27 +0900

    [Fix]class_list, class_up, class_view 코드 수정

---
+++ admin/class/class_list.php
@@ -0,0 +1,64 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/attention/admin/inc/dbcon.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/attention/admin/inc/header.php';
+?>
+
+<div>
+  <h1 class="h1 mt-5 class_m_pd text-center">강좌리스트</h1>
+  
+  <!-- 카테고리 관리 & 검색 form -->
+  <form action="">
+    <div class="d-flex justify-content-between class_sm_m">
+      <span class="btn btn-primary">카테고리 관리</span>
+      <span class="btn btn-primary">강좌 등록</span>
+    </div>
+    <div class="d-flex justify-content-between class_sm_m">
+      <span class="d-flex">
+        <select class="form-select" aria-label="Default select example"><option selected>대분류</option></select>
+        <select class="form-select class_sm_ml" aria-label="Default select example"><option selected>중분류</option></select>
+        <select class="form-select class_sm_ml" aria-label="Default select example"><option selected>소분류</option></select>
+      </span>
+      <span>
+        <input type="text" class="form-control" name="search_form" id="search_form">
+        <button class="btn btn-primary class_sm_ml">검색</button>
+      </span>
+    </div>
+
+  </form>
+  <!-- /카테고리 관리 & 검색 form -->
+  <!-- 강좌 리스트 -->
+  <form action="" method="GET">
+    <table class="table class_table">
+      <tbody>
+        <tr>
+          <td>더미 텍스트</td>
+          <td>
+            더미 텍스트
+          </td>
+          <td>
+            <div class="form-check form-switch">
+              <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+              <label class="form-check-label" for="flexSwitchCheckDefault">강좌 공개</label>
+            </div>
+          
+            <div>
+              <i class="bi bi-pencil-square icon_mint"></i>
+              <i class="bi bi-trash-fill icon_red"></i>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- /강좌 리스트 -->
+    <!-- pagenation -->
+    <nav aria-label="page_nation">
+      <ul class="pagination">
+        <!-- <li><a href=""></a></li>           -->
+      </ul>
+    </nav> 
+    <!-- /pagenation -->
+  </form>
+
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/attention/admin/inc/footer.php';
+?>
