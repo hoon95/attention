5245c6fe99a83255e65eb7059f063eaf15290e9e
rltjdms <euni48730@gmail.com>
2023-08-29 15:45:54 +0900

    [Add] 모달부분 작업

--- admin/coupon/js/coup.js
+++ admin/coupon/js/coup.js
@@ -1,3 +1,8 @@
 $(".coup_img").click(function() {
     $(".coup_hidden").trigger("click");
 })
+
+const myModalEl = document.getElementById("myModal");
+myModalEl.addEventListener("shown.bs.modal", function() {
+    $(this)
+});
