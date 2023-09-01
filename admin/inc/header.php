05696863461aeabc4f7139949296b49b7d05203a
hoon95 <gmline_@naver.com>
2023-09-01 17:26:20 +0900

    [File] common file 동기화

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -13,6 +13,9 @@
   <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/attention/admin/css/common.css">
 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 </head>
 <body>
 <div class="background d-flex">
@@ -21,7 +24,7 @@
         <img src="/attention/admin/img/coderabbit_logo.svg" alt="">
     </div>
     <ul>
-      <li><a href="/attention/admin/index.php"><i class="bi bi-grid-1x2-fill"></i> 대시보드</a></li>
+      <li><a href="/attention/admin/dashboard/index.php"><i class="bi bi-grid-1x2-fill"></i> 대시보드</a></li>
       <li><a href=""><i class="bi bi-bar-chart"></i> 매출 관리</a></li>
       <li><a href="/attention/admin/class/class_list.php"><i class="bi bi-mortarboard-fill"></i> 강좌 관리</a></li>
       <li><a href="/attention/admin/coupon/coupon_list.php"><i class="bi bi-credit-card-2-front"></i> 쿠폰 관리</a></li>
@@ -30,7 +33,7 @@
     </ul>
   </nav>
   <div class="main">
-    <div class="profile">
+    <div class="profile text-end">
       <i class="bi bi-bell icon_gray"></i>
       <i class="bi bi-emoji-sunglasses icon_gray"></i>
       <span class="tt_03">admin</span>
