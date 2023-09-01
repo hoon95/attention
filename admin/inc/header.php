b98e722db079a139e55ee2e9d9b48177e732c0cc
hoon95 <gmline_@naver.com>
2023-09-01 20:59:25 +0900

    [Add] 로그아웃 태그 추가

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -24,18 +24,22 @@
     <div class="logo">
         <img src="/attention/admin/img/coderabbit_logo.svg" alt="">
     </div>
-    <ul>
-      <li><a href="/attention/admin/dashboard/index.php"><i class="bi bi-grid-1x2-fill"></i> 대시보드</a></li>
-      <li><a href=""><i class="bi bi-bar-chart"></i> 매출 관리</a></li>
-      <li><a href="/attention/admin/class/class_list.php"><i class="bi bi-mortarboard-fill"></i> 강좌 관리</a></li>
-      <li><a href="/attention/admin/coupon/coupon_list.php"><i class="bi bi-credit-card-2-front"></i> 쿠폰 관리</a></li>
-      <li><a href=""><i class="bi bi-person-fill"></i> 회원 관리</a></li>
-      <li><a href="/attention/admin/notice/notice.php"><i class="bi bi-layout-text-sidebar-reverse"></i> 게시판 관리</a></li>
-    </ul>
+    <div class="d-flex flex-column aside_title">
+      <ul>
+        <li><a href="/attention/admin/dashboard/index.php"><i class="bi bi-grid-1x2-fill"></i> 대시보드</a></li>
+        <li><a href=""><i class="bi bi-bar-chart"></i> 매출 관리</a></li>
+        <li><a href="/attention/admin/class/class_list.php"><i class="bi bi-mortarboard-fill"></i> 강좌 관리</a></li>
+        <li><a href="/attention/admin/coupon/coupon_list.php"><i class="bi bi-credit-card-2-front"></i> 쿠폰 관리</a></li>
+        <li><a href=""><i class="bi bi-person-fill"></i> 회원 관리</a></li>
+        <li><a href="/attention/admin/notice/notice.php"><i class="bi bi-layout-text-sidebar-reverse"></i> 게시판 관리</a></li>
+      </ul>
+      <a href="/attention/logout.php" class="text2 logout"><i class="bi bi-door-closed icon_gray"></i><span>out</span></a>
+      
+    </div>
   </nav>
   <div class="main">
     <div class="profile text-end">
       <i class="bi bi-bell icon_gray"></i>
       <i class="bi bi-emoji-sunglasses icon_gray"></i>
-      <span class="tt_03">admin</span>
+      <span class="text2">admin</span>
     </div>
