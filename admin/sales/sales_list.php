047cedff304b0f53f3659dca31b7cf910316f4a0
hoon95 <gmline_@naver.com>
2023-09-04 23:39:49 +0900

    [File] 매출관리 파일 추가

---
+++ admin/sales/sales_list.php
@@ -0,0 +1,11 @@
+<?php
+    require_once $_SERVER['DOCUMENT_ROOT'].'/attention/admin/inc/header.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/attention/admin/inc/admin_check.php';
+
+?>
+
+
+
+<?php
+    require_once $_SERVER['DOCUMENT_ROOT'].'/attention/admin/inc/footer.php';
+?>
