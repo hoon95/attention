2f82d07168fa876c46c0622693b82941b6402e7f
hoon95 <66771509+hoon95@users.noreply.github.com>
2023-09-17 01:20:40 +0900

    [File] 메인페이지 js 추가

---
+++ user/js/main.js
@@ -0,0 +1,46 @@
+const mainSwiper = new Swiper(".banner-slide", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination", // 숫자가 표시될 영역을 지정합니다.
+      type: "fraction", // 숫자 형식으로 표시합니다.
+    },
+    navigation: {
+      nextEl: ".main-next",
+      prevEl: ".main-prev",
+    },
+});
+
+
+/* Banner pager */
+$(".main-slide-btn").click(function (e) {
+  let $this = $(this);
+  $this.toggleClass("active");
+
+  if($this.hasClass("active")) {
+    $(".main-stop").hide();
+    $(".main-play").show();
+    mainSwiper.autoplay.stop();
+  } else {
+    $(".main-stop").show();
+    $(".main-play").hide();
+    mainSwiper.autoplay.start();
+  }
+});
+/* /Banner pager */
+
+const newSwiper = new Swiper(".new-slide", {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 24,
+    slidesPerGroup: 2,
+    navigation: {
+      nextEl: ".new .bi-chevron-left",
+      prevEl: ".new .bi-chevron-right",
+    },
+});
