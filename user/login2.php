edb1ded50a146ad6c397584155bfac7cc36c98f5
jijihui22 <96766525+jijihui22@users.noreply.github.com>
2023-09-18 18:51:39 +0900

    [Fix]header 스타일 , login 이미지 코드 수정

--- user/login2.php
+++ user/login2.php
@@ -1,17 +1,25 @@
 <?php
+  $login_css = '<link rel="stylesheet" href="/attention/user/css/login.css">';
   require_once $_SERVER['DOCUMENT_ROOT'].'/attention/user/inc/header.php';
+  
+  if(isset($_SESSION['UID'])){
+    echo "<script>alert('이미 로그인 하셨습니다')
+          location.href = '/attention/user/index.php';</script>";
+  }
+  
+
 ?>
 <div class="container">
       <div class="login">
         <h1 class="">로그인</h1>
-        <form>
+        <form method="POST">
           <div>
             <input type="text" class="form-control login_id" placeholder="아이디">
           </div>
           <div>
             <span class="seach">
-              <input type="password" name="search" id="search" class="form-control" placeholder="비밀번호">
-              <button type="button"><i class="bi bi-eye-slash icon_gray"></i></button>
+              <input type="password" name="search" id="search" class="search form-control" placeholder="비밀번호">
+              <button type="button"><i class="pw_icon bi bi-eye-slash icon_gray"></i></button>
             </span>
           </div>
           <button class="btn btn-primary login_button">로그인</button>
@@ -25,10 +33,10 @@
         </div>
         <h5 class="text4">간편 로그인</h5>
           <ul class="login_sns d-flex justify-content-center">
-            <li><a href="#"><img src="./img/main/kakaotalk_logo.svg" alt="kakaotalk logo"></a></li>
-            <li><a href="#"><img src="./img/main/band_logo.svg" alt="band logo"></a></li>
-            <li><a href="#"><img src="./img/main/instagram_logo.svg" alt="instagram logo"></a></li>
-            <li><a href="#"><img src="./img/main/fackbook_logo.svg" alt="facebook logo"></a></li>
+            <li><a href="#"><img src="./img/main/kakaotalk_logo.png" alt="kakaotalk logo"></a></li>
+            <li><a href="#"><img src="./img/main/band_logo.png" alt="band logo"></a></li>
+            <li><a href="#"><img src="./img/main/instagram_logo.png" alt="instagram logo"></a></li>
+            <li><a href="#"><img src="./img/main/facebook_logo.png" alt="facebook logo"></a></li>
           </ul>
       </div>
     </div>
