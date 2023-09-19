7d3c46e141a214e581788f67232171691e572aaa
HyeYoungee <137126899+HyeYoungee@users.noreply.github.com>
2023-09-19 12:19:44 +0900

    [Add] 공지사항 페이지 php코드로 변경

--- user/index.php
+++ user/index.php
@@ -3,9 +3,149 @@
 
 ?>
 
-<a href="/attention/user/logout.php" class="btn btn-danger">로그아웃</a>
-<a href="/attention/user/cart/cart.php" class="btn btn-primary">장바구니</a>
+<link rel="stylesheet" href="/attention/user/css/main.css">
 
+<!-- 혜영 start -->
+<section class="best main_mg_t">
+      <div class="container_cr">
+        <div class="d-flex align-items-end justify-content-between pt-5">
+          <h2 class="tt_00">코드래빗의 BEST 강의를 만나보세요!</h2>
+          <a href="" class="text1 card_tt icon_mint">전체보기 +</a>
+        </div>
+        <p class="tt_03 pb-4">강의 볼 때마다 실력이 껑충!</p>
+        <ul class="best_list d-flex gap-4">
+          <li class="col radius_medium box_shadow">
+            <a href="" class="d-block">
+              <img src="img/main/new_1.png" alt="">
+              <div class="best_box">
+                <p class="card_tt mb-2">비전공자를 위한 진짜 입문 올인원 개발</p>
+                <p class="text5 dark_gray mb-2">그랩</p>
+                <p class="text5 dark_gray">중급 &nbsp;|&nbsp; <span class="orange">₩143,000</span></p>
+              </div>
+            </a>
+          </li>
+          <li class="col radius_medium box_shadow">
+            <a href="" class="d-block">
+              <img src="img/main/new_1.png" alt="">
+              <div class="best_box">
+                <p class="card_tt mb-2">비전공자를 위한 진짜 입문 올인원 개발 부트캠프</p>
+                <p class="text5 dark_gray mb-2">그랩</p>
+                <p class="text5 dark_gray">중급 &nbsp;|&nbsp; <span class="orange">₩143,000</span></p>
+              </div>
+            </a>
+          </li>
+          <li class="col radius_medium box_shadow">
+            <a href="" class="d-block">
+              <img src="img/main/new_1.png" alt="">
+              <div class="best_box">
+                <p class="card_tt mb-2">비전공자를 위한 진짜 입문 올인원 개발 부트캠프 비전공자를 위한 진짜 입문 올인원 개발 부트캠프</p>
+                <p class="text5 dark_gray mb-2">그랩</p>
+                <p class="text5 dark_gray">중급 &nbsp;|&nbsp; <span class="orange">₩143,000</span></p>
+              </div>
+            </a>
+          </li>
+          <li class="col radius_medium box_shadow"></li>
+        </ul>
+        <div class="total d-flex radius_medium">
+          <div class="col text-center">
+            <img src="img/main/total_member.png" alt="">
+            <p class="text1 card_tt mt-2">5,200,000 +</p>
+            <p class="mt-2">회원수</p>
+          </div>
+          <span></span>
+          <div class="col text-center">
+            <img src="img/main/total_education.png" alt="">
+            <p class="text1 card_tt mt-2">1,220,093 +</p>
+            <p class="mt-2">교육신청</p>
+          </div>
+          <span></span>
+          <div class="col text-center">
+            <img src="img/main/total_partner.png" alt="">
+            <p class="text1 card_tt mt-2">1,293 +</p>
+            <p class="mt-2">협력사</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="event container_cr main_mg_t white radius_medium">
+      <h2 class="tt_00">코드래빗 모바일 어플 출시</h2>
+      <p class="text1 mt-3 mb-3">지금 바로 다운 받고, <br>다양한 혜택과 함께 언제 어디서든 가능한 학습!!</p>
+      <div class="app_box d-flex justify-content-evenly">
+        <div>
+          <div class="d-flex gap-4 mt-5">
+            <img src="img/main/icon_playstore.png" alt="playstore 아이콘">
+            <img src="img/main/icon_qr_android.svg" alt="qr코드 아이콘">
+          </div>
+          <p class="mt-3 text-center">Android</p>
+        </div>
+        <div>
+          <div class="d-flex gap-4 mt-5">
+            <img src="img/main/icon_appstore.png" alt="appstore 아이콘">
+            <img src="img/main/icon_qr_ios.svg" alt="qr코드 아이콘">
+          </div>
+          <p class="mt-3 text-center">ios</p>
+        </div>
+      </div>
+    </section>
+    
+    <section class="pick container_cr main_mg_t d-flex gap-4">
+      <div class="pick_box col-5 radius_medium">
+        <h2 class="tt_00">코드래빗 PICK!!</h2>
+        <p class="text1 card_tt mt-2">선택이 어렵다면,<br>
+          코드래빗이 추천하는 강의를 선택해보세요 &#58;&#41;</p>
+      </div>
+      <ul class="col-7">
+        <li>
+          <p class="tt_03 ms-3">Pick! 내가 이 구역 코딩 초보</p>
+          <div class="pick_card radius_medium box_shadow p-3">
+            <a href="" class="d-flex">
+              <img src="img/main/new_6.png" alt="썸네일 이미지" class="col-4">
+              <div class="ms-4 mt-3">
+                <p class="card_tt mb-4">[코드래빗x코드캠프] 훈훈한 JavaScript</p>
+                <p class="text5 dark_gray mb-3">코드캠프</p>
+                <p class="text5 dark_gray">초급 &nbsp;|&nbsp; <span class="orange">₩22,000</span></p>
+              </div>
+            </a>
+          </div>
+        </li>
+        <li>
+          <p class="tt_03 mt-3 ms-3">Pick! 그래도 할 줄은 안다</p>
+          <div class="pick_card radius_medium box_shadow p-3">
+            <a href="" class="d-flex">
+              <img src="img/main/new_7.png" alt="썸네일 이미지" class="col-4">
+              <div class="ms-4 mt-3">
+                <p class="card_tt mb-4">JavaScript ES6+ 제대로 알아보기</p>
+                <p class="text5 dark_gray mb-3">정재남</p>
+                <p class="text5 dark_gray">중급 &nbsp;|&nbsp; <span class="orange">₩49,000</span></p>
+              </div>
+            </a>
+          </div>
+        </li>
+        <li>
+          <p class="tt_03 mt-3 ms-3">Pick! 제법 하는데?</p>
+          <div class="pick_card radius_medium box_shadow p-3">
+            <a href="" class="d-flex">
+              <img src="img/main/new_8.png" alt="썸네일 이미지" class="col-4">
+              <div class="ms-4 mt-3">
+                <p class="card_tt mb-4">Typescript with Vue 실전 프로젝트</p>
+                <p class="text5 dark_gray mb-3">성도희</p>
+                <p class="text5 dark_gray">고급 &nbsp;|&nbsp; <span class="orange">₩16,000</span></p>
+              </div>
+            </a>
+          </div>
+        </li>
+      </ul>
+    </section>
+
+    <section class="notice main_mg_t blue_Gray_back">
+      <div class="container_cr d-flex align-items-center">
+        <h3 class="text2 col-1 text-center">공지사항</h3>
+        <p class="col-8 ps-4 icon_mint"><a href="" class="d-block">코드래빗 긴급 점검 공지</a></p>
+        <p class="col-2 text-center">2023년 9월 12일</p>
+        <a href="" class="col-1 text-center icon_mint">전체보기 +</a>
+      </div>
+    </section>
 
 <?php
   require_once $_SERVER['DOCUMENT_ROOT'].'/attention/user/inc/footer.php';
