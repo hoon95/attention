01b061686d8db99f88aac49942162279ede34f2a
hoon95 <gmline_@naver.com>
2023-09-20 20:51:27 +0900

    [Fix] 내 강의실 추천강의 및 양식 수정

--- user/my_class/my_class.php
+++ user/my_class/my_class.php
@@ -8,6 +8,13 @@
   while($rs = $result->fetch_object()){
     $rsc[]=$rs;
   }
+
+  $class_query = "SELECT A.name, COUNT(A.sid) AS average_class, B.thumbnail, B.sale_end_date, B.date_val FROM sales A JOIN class B ON A.pid = B.pid GROUP BY A.name ORDER BY average_class DESC LIMIT 3";
+  $class_result = $mysqli->query($class_query);
+
+  while($class_object = $class_result->fetch_object()){
+      $class[] = $class_object;
+  }
 ?>
   <link rel="stylesheet" href="/attention/user/css/my_class.css">
 
@@ -46,16 +53,16 @@
       <h3 class="tt_02">이런 강의는 어떠세요?</h3>
       <p class="text2 gray mt-2">샘플 강의를 들어보세요!</p>
       <ul class="my_list d-flex flex-wrap justify-content-between mt-4 gap-4">
-        <?php foreach($rsc as $r){ ?>
+        <?php foreach($class as $cs){ ?>
         <li class="radius_medium box_shadow p-3 position-relative">
           <a href="">
-            <p class="card_tt mb-3"><?=  $r->name ?></p>
+            <p class="card_tt mb-3"><?=  $cs->name ?></p>
             <div class="d-flex">
-              <img src="<?= $r->thumbnail ?>" alt="썸네일 이미지" class="col-5 radius_medium">
+              <img src="<?= $cs->thumbnail ?>" alt="썸네일 이미지" class="col-5 radius_medium">
               <div class="ps-4 col">
                 <p class="text5 dark_gray py-3">코드캠프</p>
                 <p class="text5 dark_gray">기한 : 
-                  <?php if($r->sale_end_date == '0'){echo '무기한';}else{echo $r->date_val; ?>개월<?php }; ?>
+                  <?php if($cs->sale_end_date == '0'){echo '무기한';}else{echo $cs->date_val; ?>개월<?php }; ?>
                 </p>
               </div>
             </div>
