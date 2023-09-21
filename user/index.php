1d922fc35d3100de577951d30feb4f5559ec24c2
HyeYoungee <137126899+HyeYoungee@users.noreply.github.com>
2023-09-21 22:08:20 +0900

    [Fix] 메인페이지 코드 강의 테이블에 맞춰 수정

--- user/index.php
+++ user/index.php
@@ -2,13 +2,19 @@
   ob_start(); 
   require_once $_SERVER['DOCUMENT_ROOT'].'/attention/user/inc/header.php';
 
-  // $pid = $_GET['pid'];
-
-  $sql = "SELECT * FROM class WHERE 1=1";
+  /* 강의 테이블 전체 최신순 불러오기 */
+  $sql = "SELECT * FROM class ORDER BY pid DESC";
   $result = $mysqli -> query($sql);
 
-  while($rs = $result -> fetch_object()){
-    $rsc[] = $rs;
+  $count = 0; // 현재 추가한 데이터 개수를 추적하는 변수
+
+  while ($rs = $result->fetch_object()) {
+    $rsc[] = $rs; // 전체 배열
+
+    if ($count < 8) { // 최근 등록 8개 배열
+      $recentAdds[] = $rs;
+      $count++;
+    }
   }
 
   /* 테이블 전체 값 가져오기 */
@@ -30,7 +36,7 @@
     $rscNotice[] = $rsNotice;
   }
   
-  // var_dump($rsc);
+  // var_dump($recentAdds);
 ?>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css">
@@ -87,115 +93,40 @@
       <li><a href=""><h3 class="mb-2"># vue-js</h3><img src="img/main/vue-dot-js.svg" alt="vue-js"></a></li>
     </ul>
   </section>
+
   <section class="new container_cr mt-5">
     <h2 class="tt_01 mg_bot">신규 클래스</h2>
     <div class="swiper new-slide over_pd">
       <!-- Additional required wrapper -->
       <div class="swiper-wrapper">
         <!-- Slides -->
-        <div class="swiper-slide">
-          <img src="img/main/new_3.png" alt="자바스크립트 ES6+ 기초 핵심 문법">
-          <div class="text_box">
-            <h3 class="text2">자바스크립트 ES6+ 기초 핵심 문법</h3>
-            <p class="text4 name gray">이룸코딩</p>
-            <p class="text4 d-flex align-items-center">
-              <span>초급</span>
-              <span class="line"></span>
-              <span class="text3 orange">무료</span>
-            </p>
-          </div>
+        <?php
+          if(isset($recentAdds)){
+            foreach($recentAdds as $item){            
+        ?>
+        <div class="swiper-slide cart_add">
+          <a href="/attention/user/class_detail_view.php?pid=<?= $item->pid; ?>" class="d-block">
+            <img src="<?= $item->thumbnail; ?>" alt="썸네일 이미지">
+            <div class="text_box">
+              <h3 class="card_tt mb-3"><?= $item->name; ?></h3>
+              <p class="text5 name dark_gray"><?= $item->teacher; ?></p>
+              <p class="text5 dark_gray"><?= ($item->level == 1) ? '초급' : (($item->level == 2) ? '중급' : '상급'); ?> &nbsp;|&nbsp; 
+                <span class="orange">₩<span class="price"><?= ($item->price_val == 0) ? '무료' : number_format($item->price_val); ?></span></span>
+              </p>
+            </div>
+          </a>
+          <button type="button" name="add_cart" class="cart_btn ctBtn" value="<?= $item->pid; ?>"><i class="bi bi-cart3 icon_mint"></i></button>
         </div>
-        <div class="swiper-slide">
-          <img src="img/main/new_7.png" alt="따라하며 배우는 리액트 A-Z">
-          <div class="text_box">
-            <h3 class="text2">따라하며 배우는 리액트 A-Z</h3>
-            <p class="text4 name gray">John Ahn</p>
-            <p class="text4 d-flex align-items-center">
-              <span>초급</span>
-              <span class="line"></span>
-              <span class="text3 orange">₩39,000</span>
-            </p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <img src="img/main/new_9.png" alt="실습 UI 개발로 배워보는 순수 javascript와 VueJS 개발">
-          <div class="text_box">
-            <h3 class="text2">실습 UI 개발로 배워보는 순수 javascript와 VueJS 개발</h3>
-            <p class="text4 name gray">김정환</p>
-            <p class="text4 d-flex align-items-center">
-              <span>초급</span>
-              <span class="line"></span>
-              <span class="text3 orange">₩44,000</span>
-            </p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <img src="img/main/new_10.png" alt="처음하는 풀스택을 위한 Flutter 부트캠프">
-          <div class="text_box">
-            <h3 class="text2">처음하는 풀스택을 위한 Flutter 부트캠프</h3>
-            <p class="text4 name gray">잔재미코딩</p>
-            <p class="text4 d-flex align-items-center">
-              <span>중급</span>
-              <span class="line"></span>
-              <span class="text3 orange">₩79,000</span>
-            </p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <img src="img/main/new_2.png" alt="[코드래빗x코드캠프] 훈훈한 JavaScript">
-          <div class="text_box">
-            <h3 class="text2">[코드래빗x코드캠프] 훈훈한 JavaScript</h3>
-            <p class="text4 name gray">코드캠프</p>
-            <p class="text4 d-flex align-items-center">
-              <span>초급</span>
-              <span class="line"></span>
-              <span class="text3 orange">₩22,000</span>
-            </p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <img src="img/main/new_5.png" alt="JavaScript ES6+ 제대로 알아보기">
-          <div class="text_box">
-            <h3 class="text2">JavaScript ES6+ 제대로 알아보기</h3>
-            <p class="text4 name gray">정재남</p>
-            <p class="text4 d-flex align-items-center">
-              <span>중급</span>
-              <span class="line"></span>
-              <span class="text3 orange">₩49,000</span>
-            </p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <img src="img/main/new_6.png" alt="Slack 클론 코딩[실시간 채팅 with React]">
-          <div class="text_box">
-            <h3 class="text2">Slack 클론 코딩[실시간 채팅 with React]</h3>
-            <p class="text4 name gray">조현영</p>
-            <p class="text4 d-flex align-items-center">
-              <span>중급</span>
-              <span class="line"></span>
-              <span class="text3 orange">₩39,000</span>
-            </p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <img src="img/main/new_8.png" alt="생활코딩 - React">
-          <div class="text_box">
-            <h3 class="text2">생활코딩 - React</h3>
-            <p class="text4 name gray">Egoing Lee</p>
-            <p class="text4 d-flex align-items-center">
-              <span>초급</span>
-              <span class="line"></span>
-              <span class="text3 orange">무료</span>
-            </p>
-          </div>
-        </div>
+        <?php
+            }
+          }
+        ?>
       </div>
     </div>
-    <i class="bi-chevron-left icon_gray"></i>
-    <i class="bi-chevron-right icon_gray"></i>
+    <i class="chevron bi-chevron-left icon_gray"></i>
+    <i class="chevron bi-chevron-right icon_gray"></i>
   </section>
 
-  <!-- 혜영 start -->
   <section class="best main_mg_t">
     <div class="container_cr">
       <div class="d-flex align-items-end justify-content-between pt-5">
@@ -204,48 +135,82 @@
       </div>
       <p class="tt_03 pb-4">강의 볼 때마다 실력이 껑충!</p>
       <ul class="best_list d-flex gap-4">
-        <li class="col radius_medium box_shadow">
-          <a href="" class="d-block">
-            <img src="img/main/new_1.png" alt="">
-            <div class="best_box">
-              <p class="card_tt mb-2">비전공자를 위한 진짜 입문 올인원 개발</p>
-              <p class="text5 dark_gray mb-2">그랩</p>
-              <p class="text5 dark_gray">중급 &nbsp;|&nbsp; <span class="orange">₩143,000</span></p>
-            </div>
-          </a>
-        </li>
-        <li class="col radius_medium box_shadow">
-          <a href="" class="d-block">
-            <img src="img/main/new_1.png" alt="">
-            <div class="best_box">
-              <p class="card_tt mb-2">비전공자를 위한 진짜 입문 올인원 개발 부트캠프</p>
-              <p class="text5 dark_gray mb-2">그랩</p>
-              <p class="text5 dark_gray">중급 &nbsp;|&nbsp; <span class="orange">₩143,000</span></p>
-            </div>
-          </a>
-        </li>
-        <li class="col radius_medium box_shadow">
-          <a href="" class="d-block">
-            <img src="img/main/new_1.png" alt="">
-            <div class="best_box">
-              <p class="card_tt mb-2">비전공자를 위한 진짜 입문 올인원 개발 부트캠프 비전공자를 위한 진짜 입문 올인원 개발 부트캠프</p>
-              <p class="text5 dark_gray mb-2">그랩</p>
-              <p class="text5 dark_gray">중급 &nbsp;|&nbsp; <span class="orange">₩143,000</span></p>
-            </div>
-          </a>
-        </li>
+
         <?php 
         foreach ($rsc as $item) {
-          if($item -> pid == 134){
+          if($item -> pid == 25){
         ?>
         <li class="cart_add col radius_medium box_shadow">
-          <a href="/attention/user/class_view.php?pid=<?= $item->pid; ?>" class="d-block">
-            <img src="<?= $item->thumbnail; ?>" alt="">
+          <a href="/attention/user/class_detail_view.php?pid=<?= $item->pid; ?>" class="d-block">
+            <img src="<?= $item->thumbnail; ?>" alt="썸네일 이미지">
             <div class="best_box">
-              <p class="card_tt mb-2"><?= $item->name; ?></p>
-              <p class="text5 dark_gray mb-2">미출력</p>
+              <p class="card_tt mb-4"><?= $item->name; ?></p>
+              <p class="text5 dark_gray mb-2"><?= $item->teacher; ?></p>
               <p class="text5 dark_gray"><?= ($item->level == 1) ? '초급' : (($item->level == 2) ? '중급' : '상급'); ?> &nbsp;|&nbsp; 
-                <span class="orange">₩<span class="price"><?= number_format($item -> price_val); ?></span></span>
+                <span class="orange">₩<span class="price"><?= ($item->price_val == 0) ? '무료' : number_format($item->price_val); ?></span></span>
+              </p>
+            </div>
+          </a>
+          <button type="button" name="add_cart" class="cart_btn ctBtn" value="<?= $item->pid; ?>"><i class="bi bi-cart3 icon_mint"></i></button>
+        </li>
+        <?php
+            }
+          }
+        ?>
+        <?php 
+        foreach ($rsc as $item) {
+          if($item -> pid == 28){
+        ?>
+        <li class="cart_add col radius_medium box_shadow">
+          <a href="/attention/user/class_detail_view.php?pid=<?= $item->pid; ?>" class="d-block">
+            <img src="<?= $item->thumbnail; ?>" alt="썸네일 이미지">
+            <div class="best_box">
+              <p class="card_tt mb-4"><?= $item->name; ?></p>
+              <p class="text5 dark_gray mb-2"><?= $item->teacher; ?></p>
+              <p class="text5 dark_gray"><?= ($item->level == 1) ? '초급' : (($item->level == 2) ? '중급' : '상급'); ?> &nbsp;|&nbsp; 
+                <span class="orange">₩<span class="price"><?= ($item->price_val == 0) ? '무료' : number_format($item->price_val); ?></span></span>
+              </p>
+            </div>
+          </a>
+          <button type="button" name="add_cart" class="cart_btn ctBtn" value="<?= $item->pid; ?>"><i class="bi bi-cart3 icon_mint"></i></button>
+        </li>
+        <?php
+            }
+          }
+        ?>
+        <?php 
+        foreach ($rsc as $item) {
+          if($item -> pid == 29){
+        ?>
+        <li class="cart_add col radius_medium box_shadow">
+          <a href="/attention/user/class_detail_view.php?pid=<?= $item->pid; ?>" class="d-block">
+            <img src="<?= $item->thumbnail; ?>" alt="썸네일 이미지">
+            <div class="best_box">
+              <p class="card_tt mb-4"><?= $item->name; ?></p>
+              <p class="text5 dark_gray mb-2"><?= $item->teacher; ?></p>
+              <p class="text5 dark_gray"><?= ($item->level == 1) ? '초급' : (($item->level == 2) ? '중급' : '상급'); ?> &nbsp;|&nbsp; 
+                <span class="orange">₩<span class="price"><?= ($item->price_val == 0) ? '무료' : number_format($item->price_val); ?></span></span>
+              </p>
+            </div>
+          </a>
+          <button type="button" name="add_cart" class="cart_btn ctBtn" value="<?= $item->pid; ?>"><i class="bi bi-cart3 icon_mint"></i></button>
+        </li>
+        <?php
+            }
+          }
+        ?>
+        <?php 
+        foreach ($rsc as $item) {
+          if($item -> pid == 30){
+        ?>
+        <li class="cart_add col radius_medium box_shadow">
+          <a href="/attention/user/class_detail_view.php?pid=<?= $item->pid; ?>" class="d-block">
+            <img src="<?= $item->thumbnail; ?>" alt="썸네일 이미지">
+            <div class="best_box">
+              <p class="card_tt mb-4"><?= $item->name; ?></p>
+              <p class="text5 dark_gray mb-2"><?= $item->teacher; ?></p>
+              <p class="text5 dark_gray"><?= ($item->level == 1) ? '초급' : (($item->level == 2) ? '중급' : '상급'); ?> &nbsp;|&nbsp; 
+                <span class="orange">₩<span class="price"><?= ($item->price_val == 0) ? '무료' : number_format($item->price_val); ?></span></span>
               </p>
             </div>
           </a>
@@ -312,16 +277,16 @@
         <p class="tt_03 ms-3">Pick! 내가 이 구역 코딩 초보</p>
         <?php 
         foreach ($rsc as $item) {
-          if($item -> pid == 134){
+          if($item -> pid == 28){
         ?>
         <div class="cart_add pick_card radius_medium box_shadow p-3 d-flex justify-content-between align-items-end">
-          <a href="/attention/user/class_view.php?pid=<?= $item->pid; ?>" class="d-flex col-11">
+          <a href="/attention/user/class_detail_view.php?pid=<?= $item->pid; ?>" class="d-flex col-11">
             <img src="<?= $item->thumbnail; ?>" alt="썸네일 이미지" class="col-4">
             <div class="ms-4 mt-3">
               <p class="card_tt mb-4"><?= $item->name; ?></p>
-              <p class="text5 dark_gray mb-3">미출력</p>
+              <p class="text5 dark_gray mb-3"><?= $item->teacher; ?></p>
               <p class="text5 dark_gray"><?= ($item->level == 1) ? '초급' : (($item->level == 2) ? '중급' : '상급'); ?> &nbsp;|&nbsp; 
-                <span class="orange">₩<span class="price"><?= number_format($item -> price_val); ?></span></span>
+                <span class="orange">₩<span class="price"><?= ($item->price_val == 0) ? '무료' : number_format($item->price_val); ?></span></span>
               </p>
             </div>
           </a>
@@ -336,16 +301,16 @@
         <p class="tt_03 mt-3 ms-3">Pick! 그래도 할 줄은 안다</p>
         <?php 
         foreach ($rsc as $item) {
-          if($item -> pid == 137){
+          if($item -> pid == 25){
         ?>
           <div class="cart_add pick_card radius_medium box_shadow p-3 d-flex justify-content-between align-items-end">
-            <a href="/attention/user/class_view.php?pid=<?= $item->pid; ?>" class="d-flex col-11">
+            <a href="/attention/user/class_detail_view.php?pid=<?= $item->pid; ?>" class="d-flex col-11">
               <img src="<?= $item->thumbnail; ?>" alt="썸네일 이미지" class="col-4">
               <div class="ms-4 mt-3">
                 <p class="card_tt mb-4"><?= $item->name; ?></p>
-                <p class="text5 dark_gray mb-3">미출력</p>
+                <p class="text5 dark_gray mb-3"><?= $item->teacher; ?></p>
                 <p class="text5 dark_gray"><?= ($item->level == 1) ? '초급' : (($item->level == 2) ? '중급' : '상급'); ?> &nbsp;|&nbsp; 
-                  <span class="orange">₩<span class="price"><?= number_format($item -> price_val); ?></span></span>
+                  <span class="orange">₩<span class="price"><?= ($item->price_val == 0) ? '무료' : number_format($item->price_val); ?></span></span>
                 </p>
               </div>
             </a>
@@ -360,16 +325,16 @@
         <p class="tt_03 mt-3 ms-3">Pick! 제법 하는데?</p>
         <?php 
         foreach ($rsc as $item) {
-          if($item -> pid == 136){
+          if($item -> pid == 26){
         ?>
         <div class="cart_add pick_card radius_medium box_shadow p-3 d-flex justify-content-between align-items-end">
-          <a href="/attention/user/class_view.php?pid=<?= $item->pid; ?>" class="d-flex col-11">
+          <a href="/attention/user/class_detail_view.php?pid=<?= $item->pid; ?>" class="d-flex col-11">
             <img src="<?= $item->thumbnail; ?>" alt="썸네일 이미지" class="col-4">
             <div class="ms-4 mt-3">
               <p class="card_tt mb-4"><?= $item->name; ?></p>
-              <p class="text5 dark_gray mb-3">미출력</p>
+              <p class="text5 dark_gray mb-3"><?= $item->teacher; ?></p>
               <p class="text5 dark_gray"><?= ($item->level == 1) ? '초급' : (($item->level == 2) ? '중급' : '상급'); ?> &nbsp;|&nbsp; 
-                <span class="orange">₩<span class="price"><?= number_format($item -> price_val); ?></span></span>
+                <span class="orange">₩<span class="price"><?= ($item->price_val == 0) ? '무료' : number_format($item->price_val); ?></span></span>
               </p>
             </div>
           </a>
